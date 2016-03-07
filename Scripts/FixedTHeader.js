d1d1f69dcfeb477817964170cf74a1a98eee6cc6
Soheil Karami <sohil_ww@ymail.com>
2016-03-08 01:43:53 +0330

    First Commit
    This IS First Commit from Fixed Table Header With Jquery

---
+++ Scripts/FixedTHeader.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="jquery-1.9.1.js" />
+$.fn.extend({
+    fixedTable: function() {
+        var tbl = $(this);
+        var firstRow = $("tr", tbl).eq(0);
+        if (!firstRow.exsist()) {
+            firstRow = tbl.find("tr").eq(0);
+        }
+        console.log(firstRow.html());
+        var secondrow = $("tr", tbl).eq(1);
+        if (secondrow.exsist()) {
+            console.log(tbl.offset().top);
+            $(document).scroll(function(e) {
+                setToFixe(firstRow, secondrow,tbl);
+            });
+        }
+    }
+
+});
+function setToFixe(firstRow, secondrow,tbl) {
+    if ($(document).scrollTop() > tbl.offset().top) {
+        firstRow.css({
+            position: "fixed"
+        });
+        $("td", secondrow).each(function (index, elemnet) {
+            var widthcurrenttd = $(elemnet).width();
+            console.log(widthcurrenttd);
+            $("td", firstRow).eq(index).width(widthcurrenttd);
+        });
+    } else {
+        firstRow.css({
+            position: "static"
+        });
+    }
+}
+$.fn.exsist=function() {
+    return this.length !== 0;
+}
